4d57f0fac230b5840380913013c1918b4fc2bdad
redemptive <ewan_forbes@yahoo.co.uk>
2021-04-24 21:16:23 +0100

    Added whitelisted cidr block to outputs

--- aws/games-rig/outputs.tf
+++ aws/games-rig/outputs.tf
@@ -2,3 +2,7 @@ output "instance_ip_addr" {
   description = "rig_ip"
   value = aws_instance.rig_instance.public_ip
 }
+
+output "whitelisted" {
+    value = "${var.user_cidr}:${var.rig_whitelist_port}"
+}
